96c4f31e9ea6dcdffa333fa918a6a58a987d58e9
ArkoSammy12 <jamesvillarreal12@gmail.com>
2024-06-23 18:35:38 -0500

    Update to 1.21

--- src/main/java/xd/arkosammy/nodura/mixin/ItemStackMixin.java
+++ src/main/java/xd/arkosammy/nodura/mixin/ItemStackMixin.java
@@ -14,16 +14,16 @@ import xd.arkosammy.nodura.NoDuraMode;
 @Mixin(ItemStack.class)
 public class ItemStackMixin {
 
-    @ModifyExpressionValue(method = "damage(ILnet/minecraft/util/math/random/Random;Lnet/minecraft/server/network/ServerPlayerEntity;)Z", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;isDamageable()Z"))
-    private boolean onItemStackDamagedByNonEntity(boolean original, @Local @Nullable ServerPlayerEntity player){
+    @ModifyExpressionValue(method = "damage(ILnet/minecraft/server/world/ServerWorld;Lnet/minecraft/server/network/ServerPlayerEntity;Ljava/util/function/Consumer;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;isDamageable()Z"))
+    private boolean onItemStackDamagedByNonEntity(boolean original, @Local(argsOnly = true) @Nullable ServerPlayerEntity player){
         if(player == null){
             return original;
         }
         return original && ((DoDurabilityAccessor)player).nodura$getDurabilityMode() == NoDuraMode.DoDurability;
     }
 
-    @ModifyExpressionValue(method = "damage(ILnet/minecraft/entity/LivingEntity;Ljava/util/function/Consumer;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;isDamageable()Z"))
-    private <T extends LivingEntity> boolean onItemStackDamagedByEntity(boolean original, @Local T entity){
+    @ModifyExpressionValue(method = "damage(ILnet/minecraft/item/ItemConvertible;Lnet/minecraft/entity/LivingEntity;Lnet/minecraft/entity/EquipmentSlot;)Lnet/minecraft/item/ItemStack;", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;isDamageable()Z"))
+    private <T extends LivingEntity> boolean onItemStackDamagedByEntity(boolean original, @Local(argsOnly = true) T entity){
         if(!(entity instanceof ServerPlayerEntity player)){
             return original;
         }
